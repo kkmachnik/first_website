37d843994139fb1b426bda50d5b2a9bb2e968c7b
Karolina Machnik <machnik.karolina98@gmail.com>
2024-04-08 17:26:27 +0200

    visual upgrade

--- script.js
+++ script.js
@@ -15,28 +15,44 @@ myImg.onclick = zoomIn;
 
 
 const navButtons = document.getElementsByClassName("navigation-option");
+
 for (let i = 0; i < navButtons.length; i++) {
     navButtons[i].addEventListener("mouseover", event => {
-        event.target.style.color = "pink";
+        event.target.style.color = "#CDCDCD";
     });
 
     navButtons[i].addEventListener("mouseout", event => {
-        event.target.style.color = "#255957";
+        event.target.style.color = "#8F6593";
     });
 }
 
 
 
+function sectionScroll(id, offset) {
+    const element = document.getElementById(id);
+    const offsetTop = element.getBoundingClientRect().top + window.scrollY;
 
-const sections = document.getElementsByClassName("container");
+    window.scroll({
+        top: offsetTop + offset,
+        behavior: 'smooth'
+    });
+}
 
-Array.from(sections).forEach(section => {
-    section.addEventListener("click", function() {
-           const sectionTop = section.offsetTop + window.scrollY;
+function navScrollHandler(event) {
+    event.preventDefault();
+    const id = this.querySelector('a').getAttribute('href').replace("#", "");
+    const offset = -50;
+    sectionScroll(id, offset);
+};
 
-            window.scroll({
-                top: sectionTop - 100,
-                behavior: 'smooth'
-            })
-    })
+
+
+Array.from(navButtons).forEach(button => {
+    button.addEventListener("click", navScrollHandler)
 });
+
+const buttonHero = document.getElementById("button-hero");
+buttonHero.addEventListener("click",  navScrollHandler);
+
+const buttonLogo = document.getElementById("logo");
+buttonLogo.addEventListener("click",  navScrollHandler);
